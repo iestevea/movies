fabad80f3e169384fefe82e01d225cbbececef50
Iciar Esteve <iciarisabel.estevearenaza@soprasteria.com>
2018-04-25 13:49:12 +0200

    feat: add searcher with carrousel behind

--- js/utils.js
+++ js/utils.js
@@ -1,37 +1,74 @@
 function behaviour() {
-  var movies;
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET","http://localhost:3000/peliculas/",true);
-  movies = xhr.onload = function() {
-    //resolve promesa
-    return JSON.parse(xhr.responseText);
-  };
-  xhr.send();
-  console.log(movies);
-  
+    moviesBehaviour();
+    imageCarrouselBehaviour();
 }
 
-// function getMovies() {
-//   var movies;
-//   var xhr = new XMLHttpRequest();
-//   xhr.open("GET","http://localhost:3000/peliculas/",true);
-//   xhr.onload = function() {
-//     movies = JSON.parse(xhr.responseText);
-//   };
-//   xhr.send();
-//   return movies;
-// }
+function moviesBehaviour() {
+  var movies = [];
+  movies = getMovies().then((arrayMovies) => {
+    movies = arrayMovies;
+    showMovies(arrayMovies);
+  });
+}
+
+function getMovies() {
+  var promise = new Promise( (resolve,reject) => {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET","http://localhost:3000/peliculas/",true);
+    xhr.onreadystatechange = (() => {
+      if(xhr.status == 200 && xhr.readyState == 4) {
+        resolve(JSON.parse(xhr.responseText));
+      } 
+    });
+    xhr.send();
+  });
+  return promise;
+}
 
 function showMovies (movies) {
   var contentMovies = document.getElementsByClassName("content-cards")[0];
-  arrayMovie.forEach(movie => {
+  movies.forEach(movie => {
     var cardMovie = document.createElement("div");
     cardMovie.setAttribute("class","film-card");
-    cardMovie.innerHTML = getHTMLContentMovie();
+    cardMovie.innerHTML = getHTMLContentMovie(movie);
     contentMovies.appendChild(cardMovie);
   });
 }
 
-function getHTMLContentMovie() {
-  return ``;
+function getHTMLContentMovie(movie) {
+  return `<img src="${movie.cover}" alt="">
+  <p class="film-title">${movie.title}</p>
+  <p class="film-author">${movie.author}</p>`;
+}
+
+function imageCarrouselBehaviour() {
+  
+  var position = 0;
+  window.setInterval(setImageBackGround,5000);
+
+  function setImageBackGround() {
+    var contentHeader = document.getElementsByClassName("content-header")[0];
+      switch(position) {
+        case 0:
+          contentHeader.classList.remove(`bg${position}`);
+          position++;
+          contentHeader.classList.add(`bg${position}`);
+          break;
+        case 1:
+          contentHeader.classList.remove(`bg${position}`);
+          position++;
+          contentHeader.classList.add(`bg${position}`);
+          break;
+        case 2:
+          contentHeader.classList.remove(`bg${position}`);
+          position++;
+          contentHeader.classList.add(`bg${position}`);
+          break;
+        case 3:
+          contentHeader.classList.remove(`bg${position}`);
+          position = 0;
+          contentHeader.classList.add(`bg${position}`);
+          break;
+      }
+  }
 }
